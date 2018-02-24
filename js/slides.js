a7d9027d4b77d053e364c20ad4983fe9dccd9db0
Jonathan Gin <jonathangin52@gmail.com>
2018-02-24 15:17:54 -0500

    Added images

--- js/slides.js
+++ js/slides.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
     dots: true,
   });
 
+  $('#exec_slides').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+    centerMode: true,
+    centerPadding: '150px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    lazyLoad: 'ondemand'
+  });
+
 });
+//pqxz4655
